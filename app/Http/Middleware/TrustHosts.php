82ca8574a858327761985a3614c68bb12fa1634b
apple <apple@MacBook-Pro-apple.local>
2023-08-31 18:59:42 +0300

    test

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Middleware\TrustHosts as Middleware;
 
 class TrustHosts extends Middleware
 {
+    protected $proxies = '*';
+    protected $headers = Request:: HEADER_X_FORWARDED_AWS_ELB;
+
     /**
      * Get the host patterns that should be trusted.
      *
