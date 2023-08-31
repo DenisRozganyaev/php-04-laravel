beba980b5e3a16b2219f3d06685b602f51235a70
apple <apple@MacBook-Pro-apple.local>
2023-08-31 21:09:08 +0300

    fix

--- resources/views/navs/admin/side_navigation.blade.php
+++ resources/views/navs/admin/side_navigation.blade.php
@@ -4,9 +4,11 @@
         <i class="absolute top-0 right-0 hidden p-4 opacity-50 cursor-pointer fas fa-times text-slate-400 xl:hidden"
            sidenav-close></i>
         <a class="block px-8 py-6 m-0 text-sm whitespace-nowrap text-slate-700" href="{{ route('home') }}">
-            <img src="{{ Vite::asset('resources/images/admin/logo-ct.png') }}"
-                 class="inline h-full max-w-full transition-all duration-200 ease-nav-brand max-h-8"
-                 alt="main_logo"/>
+            @env('local')
+                <img src="{{ Vite::asset('resources/images/admin/logo-ct.png') }}"
+                     class="inline h-full max-w-full transition-all duration-200 ease-nav-brand max-h-8"
+                     alt="main_logo"/>
+            @endenv
             <span class="ml-1 font-semibold transition-all duration-200 ease-nav-brand">Go to Website</span>
         </a>
     </div>
